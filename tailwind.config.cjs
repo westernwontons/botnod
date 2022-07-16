59f43803b926cdb76323fe6fc6246b79b6d7e101
Nagy Botond <botondnagy98@icloud.com>
2022-07-16 19:29:51 +0300

    v2.0 is going live

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'code-mono': ['IBMPlexMono-Regular'],
+        'code-mono-bold': ['IBMPlexMono-Bold'],
+      },
+    },
+  },
+  plugins: [],
+};
