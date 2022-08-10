9dcfd2e6c1f6ecf52a3a07d62d6a5d896894df38
Nagy Botond <botondnagy98@icloud.com>
2022-08-11 00:19:16 +0300

    update: astro 1.0, trying out new image component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
-import { astroImageTools } from 'astro-imagetools';
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind({
-      config: { applyBaseStyles: false },
+      config: {
+        applyBaseStyles: false,
+      },
     }),
-    astroImageTools,
+    image(),
   ],
 });
